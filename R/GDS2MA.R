5f3cbc9d76d02f1c8b9814d5f407eaf25584d8e6
sdavis2@mail.nih.gov <sdavis2@mail.nih.gov@bc3139a8-67e5-0310-9ffc-ced21a209358>
2005-12-30 19:38:11 +0000

    Release 1.5.1
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOquery@15542 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/GDS2MA.R
@@ -0,0 +1,38 @@
+"GDS2MA" <-
+  function(GDS,do.log2=FALSE,GPL=NULL) {
+    require(limma)
+    if(is.null(GPL)) {
+      GPL <- getGEO(Meta(GDS)$platform)
+    }
+    ord.table <- match(Table(GDS)[,1],Table(GPL)[,1])
+                                        # exclude non-numeric columns
+    inc.columns <- grep('GSM',colnames(Table(GDS)))
+    if(do.log2) {
+      M <- as.matrix(log2(Table(GDS)[ord.table,inc.columns]))
+    } else {
+      M <- as.matrix(Table(GDS)[ord.table,inc.columns])
+    }
+    MA <- new('MAList',list(M=M,
+                            A=NULL,
+                            targets=Columns(GDS),
+                            genes=Table(GPL),
+                            notes=Meta(GDS)
+                            ))
+    return(MA)
+  }
+
+"GDS2eSet" <-
+  function(GDS,do.log2=FALSE) {
+    require(Biobase)
+                                        # exclude non-numeric columns
+    inc.columns <- grep('GSM',colnames(Table(GDS)))
+    if(do.log2) {
+      expr <- as.matrix(log2(Table(GDS)[,inc.columns]))
+    } else {
+      expr <- as.matrix(Table(GDS)[,inc.columns])
+    }
+    rownames(expr) <- Table(GDS)$ID
+    pheno <- new('phenoData',pData=Columns(gds),varLabels=as.list(colnames(Columns(gds))))
+    eset <- new('exprSet',exprs=expr,phenoData=pheno)
+    return(eset)
+  }
