eb4ac17c5c4c49e2e5d30b0afd09f76d75272f60
sdavis2@mail.nih.gov <sdavis2@mail.nih.gov@bc3139a8-67e5-0310-9ffc-ced21a209358>
2006-02-15 14:16:07 +0000

    Version 1.5.5 	* Added GSElimits to getGEO to allow for parsing of large datasets in pieces 	* fixed vignette to include Biobase-compliant exprSet 	* changed vignette in the GSE2eSet section to use "match" for more safely dealing with probeset ordering differences between GPLs and GSMs
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOquery@16279 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getGEO.R
+++ R/getGEO.R
@@ -1,7 +1,13 @@
 getGEO <- function(GEO=NULL,
                    filename=NULL,
-                   destdir=tempdir()) {
+                   destdir=tempdir(),
+                   GSElimits=NULL) {
   filename <- filename
+  if(!is.null(GSElimits)) {
+    if(length(GSElimits)!=2) {
+      stop('GSElimits should be an integer vector of length 2, like (1,10) to include GSMs 1 through 10')
+    }
+  }
   if(is.null(GEO) & is.null(filename)) {
     stop("You must supply either a filename of a GEO file or a GEO accession")
   }
@@ -14,7 +20,7 @@ getGEO <- function(GEO=NULL,
   } else {
     con <- file(filename,'r')
   }
-  ret <- parseGEO(con)
+  ret <- parseGEO(con,GSElimits)
   close(con)
   return(ret)
 }
