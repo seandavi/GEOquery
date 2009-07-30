bafca16d46c8491c19a15701d077a66d5e58e280
Sean Davis <sdavis2@mail.nih.gov>
2009-07-30 03:24:58 +0000

    Adding more tests
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOquery@40903 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/test/runit.GEOquery.R
+++ inst/test/runit.GEOquery.R
@@ -1,8 +1,11 @@
 test.getGEO_GDS <- function() {
   gds <- getGEO(filename=system.file("extdata/GDS507.soft.gz",package="GEOquery"))
-  checkEquals(class(gds)[[1]],'GDS',msg="GDS class check")
+   checkEquals(class(gds)[[1]],'GDS',msg="GDS class check")
   checkEquals(dim(Table(gds))[1],22645,msg="GDS row count")
   checkEquals(dim(Table(gds))[2],19,msg="GDS column count")
+  checkEquals(dim(Columns(gds)),c(17,4),msg="GDS Column method")
+  checkEquals(class(dataTable(gds))[[1]],"GEODataTable",msg="GDS dataTable method check")
+  checkEquals(Accession(gds),"GDS507",msg="GDS accession")
 }
 
 test.getGEO_GSM <- function() {
@@ -11,7 +14,9 @@ test.getGEO_GSM <- function() {
   checkEquals(dim(Table(gsm))[1],22283,msg="GSM row count")
   checkEquals(dim(Table(gsm))[2],3,msg="GSM column count")
   checkEquals(class(Meta(gsm)),"list",msg="GSM Meta() return class test")
+  checkEquals(class(dataTable(gsm))[[1]],"GEODataTable",msg="gsm dataTable method check")
   checkEquals(length(Meta(gsm)),28,msg="GSM Meta() method test")
+  checkEquals(Accession(gsm),"GSM11805",msg="GDS accession")
 }
 
 test.getGEO_GPL <- function() {
@@ -20,7 +25,9 @@ test.getGEO_GPL <- function() {
   checkEquals(dim(Table(gpl))[1],22283,msg="GPL row count")
   checkEquals(dim(Table(gpl))[2],16,msg="GPL column count")
   checkEquals(class(Meta(gpl)),"list",msg="GPL Meta() return class test")
+  checkEquals(class(dataTable(gpl))[[1]],"GEODataTable",msg="GPL dataTable method check")
   checkEquals(length(Meta(gpl)),25,msg="GPL Meta() method test")
+  checkEquals(Accession(gpl),"GPL96",msg="GDS accession")
 }
 
 
