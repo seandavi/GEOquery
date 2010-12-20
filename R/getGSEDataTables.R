1176727243d8000a4730932dfcdfecd9134e30f9
Sean Davis <sdavis2@mail.nih.gov>
2010-12-20 11:40:06 +0000

    Fixed out of memory error in xmlInternalTreeParse in getGSEDataTables
    The error was reported by James Reid
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOquery@51617 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/getGSEDataTables.R
+++ R/getGSEDataTables.R
@@ -1,6 +1,6 @@
 getGSEDataTables <- function(GSE) {
     url=sprintf("http://www.ncbi.nlm.nih.gov/projects/geo/query/acc.cgi?targ=self&form=xml&view=full&acc=%s",GSE)
-    doc1 = xmlRoot(xmlInternalTreeParse(url))
+    doc1 = xmlRoot(xmlParseDoc(url, options=HUGE, asText=FALSE))
     dTableNodes = getNodeSet(doc1,"//ns:Data-Table","ns")
     dTables = sapply(dTableNodes,function(x) {
 	    cnames=sapply(getNodeSet(x,"ns:Column/ns:Name","ns"),xmlValue)
