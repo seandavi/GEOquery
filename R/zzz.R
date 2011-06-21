5cbfa9e9feb43bcb10a7fd3abb8dc2a007d96723
sdavis2@mail.nih.gov <sdavis2@mail.nih.gov@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-06-21 19:19:50 +0000

    Merge branch 'local_2_8'
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOquery@56345 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,10 @@
 .onLoad <- function(lib,pkg) {
   require(methods)
-  message("Setting options('download.file.method'='curl')")
-  options('download.file.method'='curl')
+  if(length(grep('linux',R.Version()$os))==1) {
+    message("Setting options('download.file.method.GEOquery'='curl')")
+    options('download.file.method.GEOquery'='curl')
+  } else {
+    message("Setting options('download.file.method.GEOquery'='curl')")
+    options('download.file.method.GEOquery'='auto')
+  }
 }
