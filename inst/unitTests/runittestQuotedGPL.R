f305d9df68c3e3952b639a0bed689538a4635f3b
Sean Davis <seandavi@gmail.com>
2013-07-13 15:18:55 -0400

    converted to svUnit testing framework

---
+++ inst/unitTests/runittestQuotedGPL.R
@@ -0,0 +1,35 @@
+## Test unit 'testQuotedGPL'
+
+.setUp <-
+function () {
+	## Specific actions for svUnit: prepare context
+	if ("package:svUnit" %in% search()) {
+		.Log <- Log() ## Make sure .Log is created
+		.Log$..Unit <- "inst/unitTests/runittestQuotedGPL.R"
+		.Log$..File <- ""
+		.Log$..Obj <- ""
+		.Log$..Tag <- ""
+		.Log$..Msg <- ""
+		rm(..Test, envir = .Log)
+	}
+}
+
+.tearDown <-
+function () {
+	## Specific actions for svUnit: clean up context
+	if ("package:svUnit" %in% search()) {
+		.Log$..Unit <- ""
+		.Log$..File <- ""
+		.Log$..Obj <- ""
+		.Log$..Tag <- ""
+		.Log$..Msg <- ""
+		rm(..Test, envir = .Log)
+	}
+}
+
+"testQuotedGPL" <-
+function() {
+  gpl = getGEO('GPL4133')
+
+  checkEqualsNumeric(45220,nrow(Table(gpl)),'GPL4133 should have 45220 rows')
+}
