4322a2835b259f4365c65a104b76d4e31bc99bdd
Sean Davis <seandavi@gmail.com>
2013-10-18 22:28:54 -0400

    Removed testsuite.R--not sure where this came from

--- R/testsuite.R
+++
@@ -1,7 +0,0 @@
-runTests <- function() {
-  require(RUnit)
-  testsuite.GEOquery <- defineTestSuite("GEOquery",
-                                        dir=system.file('test',package='GEOquery'))
-  tr <- runTestSuite(testsuite.GEOquery)
-  printTextProtocol(tr)
-}
