74ad99d13da8a47d358febdc825d807b021cb456
sdavis2@mail.nih.gov <sdavis2@mail.nih.gov@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-02-09 19:07:51 +0000

    conflicts resolved while setting up Git-SVN bridge
    From: Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOquery@86255 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GDS2MA.R
+++ R/GDS2MA.R
@@ -1,11 +1,11 @@
 "GDS2MA" <-
-  function(GDS,do.log2=FALSE,GPL=NULL,AnnotGPL=TRUE) {
+  function(GDS,do.log2=FALSE,GPL=NULL,AnnotGPL=TRUE,getGPL=TRUE) {
     require(limma)
-    if(is.null(GPL)) {
+    if(!is.null(GPL) & getGPL) {
       GPL <- getGEO(Meta(GDS)$platform,AnnotGPL=AnnotGPL)
-    }
-    ord.table <- match(Table(GDS)[,1],Table(GPL)[,1])
+      ord.table <- match(Table(GDS)[,1],Table(GPL)[,1])
                                         # exclude non-numeric columns
+    }
     inc.columns <- grep('GSM',colnames(Table(GDS)))
     mat <- suppressWarnings(as.matrix(apply(Table(GDS)[,inc.columns],2,function(x) {as.numeric(as.character(x))})))
     if(do.log2) {
@@ -13,22 +13,28 @@
     } else {
       M <- mat
     }
-    MA <- new('MAList',list(M=M,
-                            A=NULL,
-                            targets=Columns(GDS),
-                            genes=Table(GPL)[ord.table,],
-                            notes=Meta(GDS)
-                            ))
+    if(is.null(GPL)) {
+        ## No GPL requested
+        return(new('MAList',list(M=M,
+                                 A=NULL,
+                                 targets=Columns(GDS),
+                                 notes=Meta(GDS)
+                                 )))
+    } else {
+        ## include GPL
+        return(new('MAList',list(M=M,
+                                 A=NULL,
+                                 targets=Columns(GDS),
+                                 genes=Table(GPL)[ord.table,],
+                                 notes=Meta(GDS)
+                                 )))}
+
     return(MA)
   }
 
 "GDS2eSet" <-
-  function(GDS,do.log2=FALSE,GPL=NULL,AnnotGPL=TRUE) {
+  function(GDS,do.log2=FALSE,GPL=NULL,AnnotGPL=TRUE,getGPL=TRUE) {
                                         # exclude non-numeric columns
-    if(is.null(GPL)) {
-      GPL <- getGEO(Meta(GDS)$platform,AnnotGPL=AnnotGPL)
-    }
-    ord.table <- match(Table(GDS)[,1],Table(GPL)[,1])
     inc.columns <- grep('GSM',colnames(Table(GDS)))
     mat <- suppressWarnings(as.matrix(apply(Table(GDS)[,inc.columns],2,
                                             function(x) {as.numeric(as.character(x))})))
@@ -44,18 +50,25 @@
     mabstract=ifelse(is.null(Meta(GDS)$description),"",Meta(GDS)$description)
     mpubmedids=ifelse(is.null(Meta(GDS)$pubmed_id),"",Meta(GDS)$pubmed_id)
     mtitle=ifelse(is.null(Meta(GDS)$title),"",Meta(GDS)$title)
-    dt <- Table(GPL)
-    rownames(dt) <- as.character(dt$ID)
-    vardt = data.frame(Column=Columns(GPL)[,1],
-      labelDescription=Columns(GPL)[,2])
-    ## GEO started using the same column names for
-    ## both GO IDs and textual descriptions, so
-    ## had to be made unique.
-    vardt[,1] <- make.unique(as.character(vardt[,1]))
-    rownames(vardt) = vardt[,1]
-    colnames(dt) <- rownames(vardt)
-    featuredata <- new('AnnotatedDataFrame',data=dt[ord.table,],
-                       varMetadata=vardt)
+    featuredata = new("AnnotatedDataFrame",data=data.frame(row.names=rownames(expr)))
+    if(getGPL | !is.null(GPL)) {
+        if(is.null(GPL)) {
+            GPL <- getGEO(Meta(GDS)$platform,AnnotGPL=AnnotGPL)
+        }
+        ord.table <- match(Table(GDS)[,1],Table(GPL)[,1])        
+        dt <- Table(GPL)
+        rownames(dt) <- as.character(dt$ID)
+        vardt = data.frame(Column=Columns(GPL)[,1],
+            labelDescription=Columns(GPL)[,2])
+        ## GEO started using the same column names for
+        ## both GO IDs and textual descriptions, so
+        ## had to be made unique.
+        vardt[,1] <- make.unique(as.character(vardt[,1]))
+        rownames(vardt) = vardt[,1]
+        colnames(dt) <- rownames(vardt)
+        featuredata <- new('AnnotatedDataFrame',data=dt[ord.table,],
+                           varMetadata=vardt)
+    } 
     eset <- new('ExpressionSet',exprs=expr,phenoData=pheno,
                 featureData=featuredata,
                 experimentData=new("MIAME",
