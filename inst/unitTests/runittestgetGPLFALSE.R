fa15b32d1d2712c6b9620b385024f05122e3f99d
sdavis2@mail.nih.gov <sdavis2@mail.nih.gov@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-02-09 20:16:53 +0000

    Commit made by the Bioconductor Git-SVN bridge. Consists of 2 commit(s).
    Commit information:
    
        Commit id: 96829c228a67fae9e2db72eee9ad82de9f9a3d2e
        Commit message:
        Added unit tests for using getGPL=FALSE in getGEO
    
        This should complete the fixes for issue #6.
        Committed by Sean Davis <seandavi at gmail.com>
        Commit date: 2014-02-09T12:15:05-08:00
    
        Commit id: e85c87c2cfb02279e8dd7e7e631c9002274b2dd2
        Commit message:
        Merge branch 'master' of github.com:seandavi/GEOquery
        Committed by Sean Davis <seandavi at gmail.com>
        Commit date: 2014-02-09T12:16:29-08:00
    
    From: Bioconductor Git-SVN Bridge <bioc-sync@bioconductor.org>
    
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOquery@86258 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/runittestgetGPLFALSE.R
+++ inst/unitTests/runittestgetGPLFALSE.R
@@ -1,8 +1,19 @@
 "testCaseGSEgetGPLFALSE" <-
 function() {
-  gse = getGEO('GSE2553')[[1]]
+  gse = getGEO('GSE2553',getGPL=FALSE)[[1]]
 
   checkTrue(validObject(gse))
-  checkEqualsNumeric(0,ncol(gse))
-  checkEqualsNumeric(12600,nrow(gse))
+  checkEqualsNumeric(0,ncol(fData(gse)))
+  checkEqualsNumeric(12600,nrow(fData(gse)))
+}
+
+
+"testCaseGDS2eSetgetGPLFALSE" <-
+function() {
+  gds = getGEO('GDS10')
+
+  eset = GDS2eSet(gds,getGPL=FALSE)
+  checkTrue(validObject(eset))
+  checkEqualsNumeric(0,ncol(fData(eset)))
+  checkEqualsNumeric(39114,nrow(fData(eset)))
 }
