d20de6073174370931c3297abd2dc9a1ec2fc5a0
Sean Davis <sdavis2@mail.nih.gov>
2015-04-18 15:29:06 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 81a9d1ff77f972de401cd45bdaf303a578a3aba1
    
        Merge branch 'master' of github.com:seandavi/GEOquery
    
    
    Commit id: 19e46d6481652a329470b5c72d88d4aabbd31330
    
        Give better error message when file does not exist and filename specified in getGEO
    
        This fixes #5.
        
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOquery@102671 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/fileOpen.R
+++ R/fileOpen.R
@@ -1,5 +1,8 @@
 fileOpen <- function(fname,...) {
   con <- NULL
+  if(!file.exists(fname)) {
+    stop(sprintf("File %s does not appear to exist.",fname))
+  }
   if(length(grep('\\.gz$',fname,perl=TRUE))>0) {
     con <- gzfile(fname,open='rt')
   } else {
