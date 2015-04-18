753bddb00a532272b68c8e41dc88f51f50521c36
Sean Davis <sdavis2@mail.nih.gov>
2015-04-18 13:22:59 +0000

    Commit made by the Bioconductor Git-SVN bridge.
    Commit id: 6b86ec2875c5d9964f0dac3b3d47bdc390d027f5
    
        Fixes #11
    
        We'll give this a try and see what breaks.  I have
        forgotten why I had to introduce this linux-specific
        funcitonality in the first place.
        
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GEOquery@102668 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,4 @@
 .onLoad <- function(lib,pkg) {
-  if(length(grep('linux',R.Version()$os))==1) {
-    packageStartupMessage("Setting options('download.file.method.GEOquery'='curl')")
-    options('download.file.method.GEOquery'='curl')
-  } else {
-    packageStartupMessage("Setting options('download.file.method.GEOquery'='auto')")
-    options('download.file.method.GEOquery'='auto')
-  }
+  packageStartupMessage("Setting options('download.file.method.GEOquery'='auto')")
+  options('download.file.method.GEOquery'='auto')
 }
