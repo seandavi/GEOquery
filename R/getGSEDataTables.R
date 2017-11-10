0239cdc9c99ac3ebe8bb158d50737e62e150acde
Sean Davis <seandavi@gmail.com>
2017-11-10 12:26:12 -0500

    Removing XML and RCurl dependencies

--- R/getGSEDataTables.R
+++ R/getGSEDataTables.R
@@ -1,17 +1,16 @@
+
+# need xml2 readr
 getGSEDataTables <- function(GSE) {
     url=sprintf("https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?targ=self&form=xml&view=full&acc=%s",GSE)
-    ## Needed to switch to httr to support https, which is now what GEO is using
-    txt = content(httr::GET(url),type='text')
-    doc1 = xmlRoot(xmlParseDoc(txt))
-    dTableNodes = getNodeSet(doc1,"//ns:Data-Table","ns")
+    doc1 = read_xml(url)
+    dTableNodes = xml_find_all(doc1,"//d1:Data-Table")
     dTables = sapply(dTableNodes,function(x) {
-	    cnames=sapply(getNodeSet(x,"ns:Column/ns:Name","ns"),xmlValue)
-		dTableText=xmlValue(getNodeSet(x,"ns:Internal-Data","ns")[[1]])
-		tc = textConnection(dTableText)
-		dTable = read.delim(tc,sep="\t",header=FALSE)
-		close(tc)
-		colnames(dTable)=cnames
-		return(dTable)
-	})
+        cnames=sapply(xml_find_all(x,"d1:Column/d1:Name"),xml_text)
+        dTableText=xml_text(xml_find_all(x,"d1:Internal-Data")[[1]])
+        browser()
+        dTable = suppressWarnings(read_tsv(dTableText, col_names = FALSE))
+        colnames(dTable)=cnames
+        return(dTable)
+    })
     return(dTables)
 }
