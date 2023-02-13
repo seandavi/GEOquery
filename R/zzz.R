8944476d463ee2fd7919b300d51507375c5f8de4
Michael Chirico <chiricom@google.com>
2023-02-13 15:06:38 -0800

    add an roxygen2 tag for future reference/autogen

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
+#' @import methods
 .onLoad <- function(lib, pkg) {
     packageStartupMessage("Setting options('download.file.method.GEOquery'='auto')")
     options(download.file.method.GEOquery = "auto")
