7c5dccba16daaca4047de6794f489920fd6bd41a
Sean Davis <seandavi@gmail.com>
2024-07-31 10:32:36 -0400

    chore: Update column names in test_search.R for consistency

--- tests/testthat/test_search.R
+++ tests/testthat/test_search.R
@@ -2,6 +2,8 @@ test_that("returns_correct_structure_when_geo_accessible", {
   result <- searchFieldsGEO()
 
   expect_s3_class(result, "data.frame")
-  expect_true(all(c("Field", "Description", "FullName") %in%
-    colnames(result)))
+  expect_true(
+    all(c("Name", "Description", "FullName") %in%
+      colnames(result))
+  )
 })
