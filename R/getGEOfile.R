7bb80c3ffc3f27fa36dd44a03d4d466b845304b0
Sean Davis <seandavi@gmail.com>
2024-07-31 12:09:29 -0400

    Convert from using curl_download to using httr2 to fix timeout issues
    Seems like setting the handle timeout on the curl handle wasn't doing
    the trick, so switching to httr2 that has a cleaner API.

--- R/getGEOfile.R
+++ R/getGEOfile.R
@@ -1,8 +1,8 @@
 #' Download a file from GEO soft file to the local machine
-#' 
+#'
 #' This function simply downloads a SOFT format file associated with the GEO
 #' accession number given.
-#' 
+#'
 #' This function downloads GEO SOFT files based on accession number.  It does
 #' not do any parsing.  The first two arguments should be fairly
 #' self-explanatory, but the last is based on the input to the acc.cgi url at
@@ -12,7 +12,7 @@
 #' Finally, 'data' downloads only the data table part of the SOFT file and is
 #' good for downloading a simple EXCEL-like file for use with other programs (a
 #' convenience).
-#' 
+#'
 #' @param GEO Character string, the GEO accession for download (eg., GDS84,
 #' GPL96, GSE2553, or GSM10)
 #' @param destdir Directory in which to store the resulting downloaded file.
@@ -28,106 +28,120 @@
 #'
 #' @importFrom utils download.file
 #'
-#' 
+#'
 #' @author Sean Davis
 #' @seealso \code{\link{getGEO}}
 #' @references http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi
 #' @keywords IO
 #' @examples
-#' 
-#'   # myfile <- getGEOfile('GDS10')
+#'
+#' # myfile <- getGEOfile('GDS10')
 #'
 #' @export
-getGEOfile <- function(GEO, destdir = tempdir(), AnnotGPL = FALSE, amount = c("full",
-    "brief", "quick", "data")) {
-    amount <- match.arg(amount)
-    geotype <- toupper(substr(GEO, 1, 3))
-    mode <- "wb"
-    GEO <- toupper(GEO)
-    stub = gsub("\\d{1,3}$", "nnn", GEO, perl = TRUE)
-    if (geotype == "GDS") {
-        gdsurl <- "https://ftp.ncbi.nlm.nih.gov/geo/datasets/%s/%s/soft/%s"
-        myurl <- sprintf(gdsurl, stub, GEO, paste0(GEO, ".soft.gz"))
-        destfile <- file.path(destdir, paste0(GEO, ".soft.gz"))
-    }
-    if (geotype == "GSE" & amount == "full") {
-        gseurl <- "https://ftp.ncbi.nlm.nih.gov/geo/series/%s/%s/soft/%s"
-        myurl <- sprintf(gseurl, stub, GEO, paste0(GEO, "_family.soft.gz"))
-        destfile <- file.path(destdir, paste(GEO, ".soft.gz", sep = ""))
-    }
-    if (geotype == "GSE" & amount != "full" & amount != "table") {
-        gseurl <- "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi"
-        myurl <- paste(gseurl, "?targ=self&acc=", GEO, "&form=text&view=", amount,
-            sep = "")
-        destfile <- file.path(destdir, paste(GEO, ".soft", sep = ""))
-        mode <- "w"
-    }
-    if (geotype == "GPL") {
-        if (AnnotGPL) {
-            gplurl <- "https://ftp.ncbi.nlm.nih.gov/geo/platforms/%s/%s/annot/%s"
-            myurl <- sprintf(gplurl, stub, GEO, paste0(GEO, ".annot.gz"))
-            destfile <- file.path(destdir, paste(GEO, ".annot.gz", sep = ""))
-            # check to see if Annotation GPL is present.  If so, use it, else
-            # move on to submitter GPL
-            res = try({
-                if (!file.exists(destfile)) {
-                  downloadFile(myurl, destfile, mode)
-                } else {
-                  message(sprintf("Using locally cached version of %s found here:\n%s ",
-                    GEO, destfile))
-                }
-            }, silent = TRUE)
-            if (!inherits(res, "try-error")) {
-                return(invisible(destfile))
-            } else {
-                message("Annotation GPL not available, so will use submitter GPL instead")
-            }
-        }
-        gseurl <- "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi"
-        myurl <- paste(gseurl, "?targ=self&acc=", GEO, "&form=text&view=", amount,
-            sep = "")
-        destfile <- file.path(destdir, paste(GEO, ".soft.gz", sep = ""))
-        mode <- "w"
-        if (!file.exists(destfile)) {
-            downloadFile(myurl, destfile, mode = mode, quiet = TRUE)
-        } else {
-            message(sprintf("Using locally cached version of %s found here:\n%s ",
-                GEO, destfile))
-        }
+getGEOfile <- function(GEO, destdir = tempdir(), AnnotGPL = FALSE, amount = c(
+                         "full",
+                         "brief", "quick", "data"
+                       )) {
+  amount <- match.arg(amount)
+  geotype <- toupper(substr(GEO, 1, 3))
+  mode <- "wb"
+  GEO <- toupper(GEO)
+  stub <- gsub("\\d{1,3}$", "nnn", GEO, perl = TRUE)
+  if (geotype == "GDS") {
+    gdsurl <- "https://ftp.ncbi.nlm.nih.gov/geo/datasets/%s/%s/soft/%s"
+    myurl <- sprintf(gdsurl, stub, GEO, paste0(GEO, ".soft.gz"))
+    destfile <- file.path(destdir, paste0(GEO, ".soft.gz"))
+  }
+  if (geotype == "GSE" & amount == "full") {
+    gseurl <- "https://ftp.ncbi.nlm.nih.gov/geo/series/%s/%s/soft/%s"
+    myurl <- sprintf(gseurl, stub, GEO, paste0(GEO, "_family.soft.gz"))
+    destfile <- file.path(destdir, paste(GEO, ".soft.gz", sep = ""))
+  }
+  if (geotype == "GSE" & amount != "full" & amount != "table") {
+    gseurl <- "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi"
+    myurl <- paste(gseurl, "?targ=self&acc=", GEO, "&form=text&view=", amount,
+      sep = ""
+    )
+    destfile <- file.path(destdir, paste(GEO, ".soft", sep = ""))
+    mode <- "w"
+  }
+  if (geotype == "GPL") {
+    if (AnnotGPL) {
+      gplurl <- "https://ftp.ncbi.nlm.nih.gov/geo/platforms/%s/%s/annot/%s"
+      myurl <- sprintf(gplurl, stub, GEO, paste0(GEO, ".annot.gz"))
+      destfile <- file.path(destdir, paste(GEO, ".annot.gz", sep = ""))
+      # check to see if Annotation GPL is present.  If so, use it, else
+      # move on to submitter GPL
+      res <- try(
+        {
+          if (!file.exists(destfile)) {
+            downloadFile(myurl, destfile, mode)
+          } else {
+            message(sprintf(
+              "Using locally cached version of %s found here:\n%s ",
+              GEO, destfile
+            ))
+          }
+        },
+        silent = TRUE
+      )
+      if (!inherits(res, "try-error")) {
         return(invisible(destfile))
+      } else {
+        message("Annotation GPL not available, so will use submitter GPL instead")
+      }
     }
-    if (geotype == "GSM") {
-        gseurl <- "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi"
-        myurl <- paste(gseurl, "?targ=self&acc=", GEO, "&form=text&view=", amount,
-            sep = "")
-        destfile <- file.path(destdir, paste(GEO, ".soft", sep = ""))
-        mode <- "w"
-    }
+    gseurl <- "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi"
+    myurl <- paste(gseurl, "?targ=self&acc=", GEO, "&form=text&view=", amount,
+      sep = ""
+    )
+    destfile <- file.path(destdir, paste(GEO, ".soft.gz", sep = ""))
+    mode <- "w"
     if (!file.exists(destfile)) {
-        downloadFile(myurl, destfile, mode)
+      downloadFile(myurl, destfile, mode = mode, quiet = TRUE)
     } else {
-        message(sprintf("Using locally cached version of %s found here:\n%s ", GEO,
-            destfile))
+      message(sprintf(
+        "Using locally cached version of %s found here:\n%s ",
+        GEO, destfile
+      ))
     }
-    # if(length(grep('\\.gz',destfile,perl=TRUE))>0) {
-    # gunzip(destfile,overwrite=TRUE,remove=TRUE) destfile <-
-    # sub('\\.gz$','',destfile) }
-    invisible(destfile)
+    return(invisible(destfile))
+  }
+  if (geotype == "GSM") {
+    gseurl <- "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi"
+    myurl <- paste(gseurl, "?targ=self&acc=", GEO, "&form=text&view=", amount,
+      sep = ""
+    )
+    destfile <- file.path(destdir, paste(GEO, ".soft", sep = ""))
+    mode <- "w"
+  }
+  if (!file.exists(destfile)) {
+    downloadFile(myurl, destfile, mode)
+  } else {
+    message(sprintf(
+      "Using locally cached version of %s found here:\n%s ", GEO,
+      destfile
+    ))
+  }
+  # if(length(grep('\\.gz',destfile,perl=TRUE))>0) {
+  # gunzip(destfile,overwrite=TRUE,remove=TRUE) destfile <-
+  # sub('\\.gz$','',destfile) }
+  invisible(destfile)
 }
 
 getGEORaw <- function(GEO, destdir = tempdir()) {
-    return(getGEOSuppFiles(GEO, baseDir = destdir))
+  return(getGEOSuppFiles(GEO, baseDir = destdir))
 }
 
 
 
 #' Gunzip a file
-#' 
+#'
 #' gunzip a file
-#' 
+#'
 #' This function was stripped out of R.utils due to breaking some stuff on the
 #' bioconductor build machine.
-#' 
+#'
 #' @param filename The filename to be unzipped
 #' @param destname The destination file
 #' @param overwrite Boolean indicating whether or not to overwrite a destfile
@@ -141,65 +155,85 @@ getGEORaw <- function(GEO, destdir = tempdir()) {
 #' @keywords IO
 #'
 #' @examples
-#' 
+#'
 #' # gunzip('file.gz',remove=FALSE)
-#' 
-#' 
+#'
 #' @export
-gunzip <- function(filename, destname = gsub("[.]gz$", "", filename), overwrite = FALSE,
+gunzip <- function(
+    filename, destname = gsub("[.]gz$", "", filename), overwrite = FALSE,
     remove = TRUE, BFR.SIZE = 1e+07) {
-    if (filename == destname)
-        stop(sprintf("Argument 'filename' and 'destname' are identical: %s", filename))
-    if (!overwrite && file.exists(destname))
-        stop(sprintf("File already exists: %s", destname))
+  if (filename == destname) {
+    stop(
+      sprintf(
+        "Argument 'filename' and 'destname' are identical: %s",
+        filename
+      )
+    )
+  }
+  if (!overwrite && file.exists(destname)) {
+    stop(sprintf("File already exists: %s", destname))
+  }
 
-    inn <- gzfile(filename, "rb")
-    on.exit(if (!is.null(inn)) close(inn))
+  inn <- gzfile(filename, "rb")
+  on.exit(if (!is.null(inn)) close(inn))
 
-    out <- file(destname, "wb")
-    on.exit(close(out), add = TRUE)
+  out <- file(destname, "wb")
+  on.exit(close(out), add = TRUE)
 
-    nbytes <- 0
-    repeat {
-        bfr <- readBin(inn, what = raw(0), size = 1, n = BFR.SIZE)
-        n <- length(bfr)
-        if (n == 0)
-            break
-        nbytes <- nbytes + n
-        writeBin(bfr, con = out, size = 1)
+  nbytes <- 0
+  repeat {
+    bfr <- readBin(inn, what = raw(0), size = 1, n = BFR.SIZE)
+    n <- length(bfr)
+    if (n == 0) {
+      break
     }
+    nbytes <- nbytes + n
+    writeBin(bfr, con = out, size = 1)
+  }
 
-    if (remove) {
-        close(inn)
-        inn <- NULL
-        file.remove(filename)
-    }
+  if (remove) {
+    close(inn)
+    inn <- NULL
+    file.remove(filename)
+  }
 
-    invisible(nbytes)
+  invisible(nbytes)
 }
 
 # internal use only
 downloadFile <- function(url, destfile, mode, quiet = TRUE) {
-    h <- curl::new_handle()
-    curl::handle_setheaders(h, `accept-encoding` = "gzip")
-    timeout_seconds <- max(getOption("timeout"), 120)
-    curl::handle_setopt(h, timeout_ms = timeout_seconds * 1000)
-    result = tryCatch({
-        curl::curl_download(url, destfile, mode = mode, quiet = quiet, handle = h)
-        return(TRUE)
-    }, error = function(e) {
-        message(e)
-        return(FALSE)
-    })
-    message("File stored at:")
-    message(destfile)
+  timeout_seconds <- max(getOption("timeout"), 120)
 
-    ## if the download failed, remove the corrupted file and report the error
-    if (!result) {
-        if (file.exists(destfile)) {
-            file.remove(destfile)
-        }
-        stop(sprintf("Failed to download %s!", destfile))
+  result <- tryCatch(
+    {
+      req <- httr2::request(url) %>%
+        httr2::req_headers(`accept-encoding` = "gzip") %>%
+        httr2::req_timeout(timeout_seconds)
+
+      resp <- httr2::req_perform(req)
+
+      if (httr2::resp_status(resp) == 200) {
+        writeBin(httr2::resp_body_raw(resp), destfile)
+        return(TRUE)
+      } else {
+        stop("Failed to download file: ", httr2::resp_status(resp))
+      }
+    },
+    error = function(e) {
+      message(e)
+      return(FALSE)
     }
-    return(0)
+  )
+
+  message("File stored at:")
+  message(destfile)
+
+  ## if the download failed, remove the corrupted file and report the error
+  if (!result) {
+    if (file.exists(destfile)) {
+      file.remove(destfile)
+    }
+    stop(sprintf("Failed to download %s!", destfile))
+  }
+  return(0)
 }
